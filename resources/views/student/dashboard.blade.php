05ace01b1b7bd2837958929be2b0867c76ac43f7
Miyuru Sathsara Priyakantha <139642904+Miyuru-SP@users.noreply.github.com>
2024-10-05 20:58:41 +0530

    student dashboard button style
    download clearance button

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -4,6 +4,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> <!-- Font Awesome -->
+
 </head>
 
 @extends('layouts.student')
@@ -55,17 +57,17 @@
                         <button type="submit" class="btn btn-primary" id="submitButton" {{ $application ? 'disabled' : '' }}>
                             {{ $application ? 'Application Submitted' : 'Submit Clearance Form' }}
                         </button>
-                    </div>
-                        <!-- Download Clearance button -->
-                    @if ($allApproved) <!-- Condition to check if all departments have approved -->
-                    <a href="{{ route('student.downloadClearancePDF') }}" class="btn btn-success" id="downloadButton">
-                        Download Clearance
-                    </a>
-                @else
-                    <button class="btn btn-secondary" disabled>
-                        Download Clearance
-                    </button>
-                @endif
+                        <br>
+                            @if ($allApproved) <!-- Condition to check if all departments have approved -->
+                                <a href="{{ route('student.downloadClearancePDF') }}" class="downloadButton" id="downloadButton">
+                                    Download Clearance
+                                </a>
+                            @else
+                                <button class="downloadButtonDisable" id="downloadButton" disabled>
+                                    Download Clearance
+                                </button>
+                            @endif
+                        </div>
                 </form>
             </div>
         </div>
